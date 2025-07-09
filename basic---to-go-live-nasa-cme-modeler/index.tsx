7faa9c35809daccb673c652c1c568509787fb843
TNRProtography <thenamesrock@gmail.com>
2025-07-10 11:50:19 +1200

    Trying to make this an installable app

--- basic---to-go-live-nasa-cme-modeler/index.tsx
+++ basic---to-go-live-nasa-cme-modeler/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -31,3 +30,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// --- ADDED: Service Worker Registration Logic ---
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
