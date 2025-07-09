e61b5ad1b60c3b009f1b5b8a18f50601c6f8faff
TNRProtography <thenamesrock@gmail.com>
2025-07-09 19:55:14 +1200

    rollback to working version

--- basic---to-go-live-nasa-cme-modeler/vite.config.ts
+++ basic---to-go-live-nasa-cme-modeler/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // This defines the main app entry point
+        main: resolve(__dirname, 'index.html'),
+        // This tells Vite to also build forecast.html as a separate page
+        forecast: resolve(__dirname, 'forecast.html'),
+      },
+    },
+  },
 })
