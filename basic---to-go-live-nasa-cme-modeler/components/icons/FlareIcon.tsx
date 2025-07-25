cca8d39493f37719dd97c0ed2d55b51c2faedfc8
TNRProtography <thenamesrock@gmail.com>
2025-07-25 13:33:47 +1200

    New cme icon

--- basic---to-go-live-nasa-cme-modeler/components/icons/FlareIcon.tsx
+++ basic---to-go-live-nasa-cme-modeler/components/icons/FlareIcon.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const FlareIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM8.547 4.505a8.25 8.25 0 1010.334 10.334.75.75 0 00-1.06-1.06 6.75 6.75 0 01-8.214-8.214.75.75 0 00-1.06-1.06z" clipRule="evenodd" />
+    {/* The group ensures both parts are treated as one icon */}
+    <g>
+      {/* The main body of the Sun */}
+      <circle cx="10" cy="12" r="8"/>
+      
+      {/* 
+        The erupting flare. This is a single closed path that creates a swoosh shape.
+        It starts on the sun's edge, curves out dramatically, and then curves back,
+        with the inner edge following the sun's circumference perfectly.
+      */}
+      <path d="M16,5.34 C19,-1, 29,6, 22,12 C29,18, 19,25, 16,18.66 A8,8 0 0 1 16,5.34 Z" />
+    </g>
   </svg>
 );
 
-// THIS IS THE CRUCIAL LINE THAT WAS MISSING
 export default FlareIcon;
